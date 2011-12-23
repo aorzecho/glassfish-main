3aea978d0173223f4c2bc801fd1f4ab7479a1eae
gmurr <gmurr@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2011-12-23 18:26:52 +0000

    git-svn-id: https://svn.java.net/svn/glassfish~svn/branches/l10n-3.1.2@51750 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- packager/resources/pkg_conf.py
+++ packager/resources/pkg_conf.py
@@ -61,14 +61,14 @@
 # example, grizzly version would look like 1.8.2-0,0. 
 
 glassfish_version="3.1.2,0-${build.id}"
-felix_version="3.0.8,0-0"
+felix_version="4.0.2,0-0"
 javadb_version="10.5.3.0,0-0"
-corba_version="3.1.0,0-30"
-jsf_version="2.1.3,0-4"
-grizzly_version="1.9.36,0-1"
-metro_version="2.2,0-4"
+corba_version="3.1.0,0-32"
+jsf_version="2.1.6,0-4"
+grizzly_version="1.9.43,0-1"
+metro_version="2.2,0-12"
 javahelp_version="2.0.2,0-1"
-shoal_version="1.6.13,0-0"
+shoal_version="1.6.15,0-0"
 
 #description
 glassfish_description="GlassFish Application Server"
